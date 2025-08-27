e83acf618d47bdb4cba0c7a9b15f0b5093885486
rukevweubio22 <ubiorukevwe@gmail.com>
2025-08-27 09:57:06 +0000

    update the jenkinsfile

--- jenkins/src/main/java/com/example/tictactoe/TicTacToe.java
+++ jenkins/src/main/java/com/example/tictactoe/TicTacToe.java
@@ -69,4 +69,10 @@ public class TicTacToe {
     }
 
     private boolean isDraw() {
-        for (in
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++)
+                if (board[i][j] == ' ')
+                    return false;
+        return true;
+    }
+}
